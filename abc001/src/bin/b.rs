d03dc36a6a55ae804b6db1fed94010a24b49d7b2
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-01 00:14:32 +0900

    Submit ABC001 B

--- abc001/src/bin/b.rs
+++ abc001/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        m: f32
+    }
+
+    let k = m / 1000.0;
+    if m < 100.0 {
+        println!("00");
+    } else if m >= 100.0 && m <= 5000.0 {
+        println!("{:02}", k * 10.0);
+    } else if m >= 6000.0 && m <= 30000.0 {
+        println!("{}", k + 50.0);
+    } else if m >= 35000.0 && m <= 70000.0 {
+        println!("{}", ((k - 30.0) / 5.0) + 80.0);
+    } else {
+        println!("89");
+    }
 }
