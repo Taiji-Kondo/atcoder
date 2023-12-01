6f01c4f0146ce79456406d85d8d718a25b967e7e
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-02 00:11:20 +0900

    Submit ABC330 A

--- abc330/src/bin/a.rs
+++ abc330/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+        a: [usize; n],
+    }
+
+    let mut answer = 0;
+
+    for i in 0..n {
+        if a[i] >= l {
+            answer = answer + 1;
+        }
+    }
+
+    println!("{}", answer);
 }
