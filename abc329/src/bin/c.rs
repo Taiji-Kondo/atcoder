e79fb9f91699a814ed3de82c15698d1d3643a21d
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 22:01:34 +0900

    Submit ABC329 C (RE)

--- abc329/src/bin/c.rs
+++ abc329/src/bin/c.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: String,
+    }
+
+    let mut answer: Vec<String> = vec![];
+
+    for i in 0..n {
+        let mut word: String = (&s[i..i+1]).to_string();
+        answer.push(word.clone());
+        for j in i + 1..n {
+            if (&s[i..i+1]).to_string() != &s[j..j+1] {
+                break;
+            }
+            let new_word: String = format!("{}{}", word, (&s[j..j+1]).to_string());
+            word = new_word.clone();
+            answer.push(new_word);
+        }
+    }
+
+    // 並び替え
+    answer.sort();
+    // 重複削除
+    answer.dedup();
+
+    println!("{:?}", answer.len());
 }
