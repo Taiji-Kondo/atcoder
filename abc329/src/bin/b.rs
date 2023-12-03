a97ca40e6ce747b4cda5cf143fa9c13ef7bd2527
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 18:20:03 +0900

    Submit ABC329 B

--- abc329/src/bin/b.rs
+++ abc329/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut first: usize = 0;
+    for i in 0..n {
+        if a[i] > first { first = a[i] }
+    }
+
+    let mut second: usize = 0;
+    for i in 0..n {
+        if second == 0 && a[i] < first {
+            second = a[i]
+        } else if a[i] < first && a[i] > second {
+            second = a[i]
+        }
+    }
+
+    println!("{}", second);
 }
+
