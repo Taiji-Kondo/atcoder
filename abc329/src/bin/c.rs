13813a33d0457ac6077c3b3ab37b2091739cf2b6
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-05 07:51:09 +0900

    (ABC329 C): Add another answer(error)

--- abc329/src/bin/c.rs
+++ abc329/src/bin/c.rs
@@ -6,25 +6,48 @@ fn main() {
         s: String,
     }
 
-    let mut answer: Vec<String> = vec![];
+    // NOTE: 別の文字が削除されてしまう
+    // let mut answer: Vec<u32> = vec![];
+    // let mut count: u32 = 0;
+    //
+    // for i in 0..n {
+    //     if i == 0 {
+    //         count += 1;
+    //         answer.push(count + 1);
+    //     } else if i == 0 || s[i..i+1] != s[i-1..i] {
+    //         answer.push(count + 1);
+    //         count = 0;
+    //     } else {
+    //         count += 1;
+    //         answer.push(count + 1);
+    //     }
+    // }
+    //
+    // // 並び替え
+    // answer.sort();
+    // // 重複削除
+    // answer.dedup();
 
-    for i in 0..n {
-        let mut word: String = (&s[i..i+1]).to_string();
-        answer.push(word.clone());
-        for j in i + 1..n {
-            if (&s[i..i+1]).to_string() != &s[j..j+1] {
-                break;
-            }
-            let new_word: String = format!("{}{}", word, (&s[j..j+1]).to_string());
-            word = new_word.clone();
-            answer.push(new_word);
-        }
-    }
-
-    // 並び替え
-    answer.sort();
-    // 重複削除
-    answer.dedup();
+    // O(n**2)以上でTLEしてしまう
+    // let mut answer: Vec<String> = vec![];
+    //
+    // for i in 0..n {
+    //     let mut word: String = (&s[i..i+1]).to_string();
+    //     answer.push(word.clone());
+    //     for j in i + 1..n {
+    //         if (&s[i..i+1]).to_string() != &s[j..j+1] {
+    //             break;
+    //         }
+    //         let new_word: String = format!("{}{}", word, (&s[j..j+1]).to_string());
+    //         word = new_word.clone();
+    //         answer.push(new_word);
+    //     }
+    // }
+    //
+    // // 並び替え
+    // answer.sort();
+    // // 重複削除
+    // answer.dedup();
 
     println!("{:?}", answer.len());
 }
