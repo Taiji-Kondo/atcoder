6374d2f4eec9c8bfb5679dea444602f7cf0fa837
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-05 08:03:28 +0900

    (ABC328 A): submit

--- abc328/src/bin/a.rs
+++ abc328/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: u32,
+        s: [u32; n],
+    }
+
+    let mut total: u32 = 0;
+    for i in 0..n {
+        if s[i] <= x {
+            total += s[i]
+        }
+    }
+    println!("{}", total);
 }
