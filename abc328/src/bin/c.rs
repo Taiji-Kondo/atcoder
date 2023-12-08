3c62058335a4466ca9fca37321c8983988b1b2a3
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-08 20:15:41 +0900

    (ABC328 C): submit(TLE)

--- abc328/src/bin/c.rs
+++ abc328/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: [usize;2],
+        s: String,
+        lr: [[usize;2];n[1]],
+    }
+
+    for i in 0..n[1] {
+        let l: usize = lr[i][0];
+        let r: usize = lr[i][1];
+        let split_s = &s[l-1..r];
+        let mut count: u32 = 0;
+
+        for i in 0..split_s.chars().count() {
+            if i + 1 < split_s.chars().count() && split_s[i..i+1] == split_s[i+1..i+2] {
+                count += 1;
+            }
+        }
+        println!("{}", count);
+    }
 }
